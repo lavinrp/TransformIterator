1a6973c5b3d9326343b88268ede59b0d8bb06f05
Ryan Lavin <lavinrp@gmail.com>
2020-01-22 20:24:43 -0500

    Initial work

---
+++ TransformIterator.h
@@ -0,0 +1,426 @@
+﻿#pragma once
+
+#include <type_traits>
+#include <iterator>
+
+namespace lagy {
+
+	template <class, class = std::void_t<> >
+	struct IsIterator : std::false_type {};
+
+	/// <summary>
+	/// Determines if the input type is an iterator.
+	/// IsIterator<input>::value is true if the input is an iterator and false if it is not
+	/// </summary>
+	template <class Iterator>
+	struct IsIterator <
+		Iterator,
+		typename std::enable_if_t<
+		!std::is_same_v<typename std::iterator_traits<Iterator>::value_type, void>
+		>
+	> : std::true_type {};
+
+	/// <summary>
+	/// Determines if the input type is an iterator.
+	/// Evaluates to true if the input is an iterator and false if it is not
+	/// </summary>
+	template <typename T>
+	constexpr inline bool IsIterator_v = IsIterator<T>::value;
+
+	namespace Detail
+	{
+
+		/// <summary>
+		/// True if random access convenience operations are supported by this iterator.
+		/// </summary>
+		template <class Iterator>
+		inline constexpr bool SupportsRandomAccess_v =
+			std::is_same_v< typename std::iterator_traits<Iterator>::iterator_category, std::random_access_iterator_tag >;
+
+		/// <summary>
+		/// True if bidirectional convenience operations are supported by this iterator.
+		/// </summary>
+		template <class Iterator>
+		inline constexpr bool SupportsBidirectional_v =
+			SupportsRandomAccess_v<Iterator> ||
+			std::is_same_v< typename std::iterator_traits<Iterator>::iterator_category, std::bidirectional_iterator_tag >;
+
+		/// <summary>
+		/// Determines the iterator category of a transform iterator based on the category of the wrapped iterator
+		/// and the result type of the transform.
+		/// </summary>
+		template <class Iterator, class UnaryOperation, class = std::void_t<>>
+		struct TransformIteratorCategory
+		{
+			using type = std::input_iterator_tag;
+		};
+
+		/// <summary>
+		/// Determines the iterator category of a transform iterator based on the category of the wrapped iterator
+		/// and the result type of the transform.
+		/// </summary>
+		template <class Iterator, class UnaryOperation>
+		struct TransformIteratorCategory<Iterator, UnaryOperation, std::enable_if_t<std::is_reference_v<std::invoke_result_t<std::decay_t<UnaryOperation>, Iterator&>>>>
+		{
+			using type = typename std::iterator_traits<Iterator>::iterator_category;
+		};
+
+		template <class Iterator, class UnaryOperation>
+		using TransformIteratorCategory_t = typename TransformIteratorCategory<Iterator, UnaryOperation>::type;
+
+		template <class Iterator, typename UnaryOperation>
+		class TransformIteratorTraitProvider
+		{
+		public:
+			// Sanity checks
+			static_assert(std::is_invocable_v<std::decay_t<UnaryOperation>, Iterator&>, "TransformIterator must be provided a UnaryOperation that operates on a reference to the type of its wrapped iterator.");
+			static_assert(IsIterator_v<Iterator>, "TransformIterator must be provided a valid iterator type to wrap.");
+
+			/// <summary>
+			/// The type of the wrapped iterator.
+			/// </summary>
+			using WrappedIteratorType = Iterator;
+
+			// std::iterator_traits types
+			using reference = std::invoke_result_t<std::decay_t<UnaryOperation>, WrappedIteratorType&>;
+			using value_type = std::remove_reference_t<reference>;
+			using difference_type = typename std::iterator_traits<WrappedIteratorType>::difference_type;
+			using pointer = value_type*;
+			using iterator_category = TransformIteratorCategory_t<WrappedIteratorType, UnaryOperation>;
+		};
+
+		template <
+			class CrtpChildIterator,
+			class WrappedIterator,
+			class UnaryOperation,
+			class = std::void_t<>
+		>
+			class TransformIteratorBidirectionalHelper : public TransformIteratorTraitProvider<WrappedIterator, UnaryOperation> {};
+
+		template <
+			class CrtpChildIterator,
+			class WrappedIterator,
+			class UnaryOperation
+		>
+			class TransformIteratorBidirectionalHelper <CrtpChildIterator, WrappedIterator, UnaryOperation, std::enable_if_t<SupportsBidirectional_v<WrappedIterator>>> :
+			public TransformIteratorTraitProvider<WrappedIterator, UnaryOperation>
+		{
+		private:
+			using TraitProvider = TransformIteratorTraitProvider<WrappedIterator, UnaryOperation>;
+
+		public:
+
+			// Iterator types
+			using typename TraitProvider::WrappedIteratorType;
+			using typename TraitProvider::value_type;
+			using typename TraitProvider::difference_type;
+			using typename TraitProvider::pointer;
+			using typename TraitProvider::reference;
+			using typename TraitProvider::iterator_category;
+
+			/// <summary>
+			/// Moves the iterator backward.
+			/// Only available if the wrapped iterator is a bidirectional iterator.
+			/// </summary>
+			/// <return> A new iterator at the position of the original before it was moved backward. </return>
+			CrtpChildIterator& operator--()
+			{
+				--getCrtpThis->getWrappedIterator();
+				return *getCrtpThis();
+			}
+
+			/// <summary>
+			/// Moves the iterator backward.
+			/// Only available if the wrapped iterator is a bidirectional iterator.
+			/// </summary>
+			/// <return> A new iterator at the position of the original before it was moved backward. </return>
+			[[nodiscard]]
+			CrtpChildIterator operator--(int)
+			{
+				CrtpChildIterator out(*this);
+				--(*getCrtpThis());
+				return out;
+			}
+
+		private:
+
+			[[nodiscard]]
+			CrtpChildIterator* getCrtpThis()
+			{
+				return static_cast<CrtpChildIterator*>(this);
+			}
+		};
+
+		template <
+			class CrtpChildIterator,
+			class WrappedIterator,
+			class UnaryOperation,
+			class = std::void_t<>
+		>
+			class TransformIteratorRandomAccessHelper : public TransformIteratorBidirectionalHelper<CrtpChildIterator, WrappedIterator, UnaryOperation> {};
+
+		template <
+			class CrtpChildIterator,
+			class WrappedIterator,
+			class UnaryOperation
+		>
+			class TransformIteratorRandomAccessHelper <CrtpChildIterator, WrappedIterator, UnaryOperation, std::enable_if_t<SupportsRandomAccess_v<WrappedIterator>>> :
+			public TransformIteratorBidirectionalHelper<CrtpChildIterator, WrappedIterator, UnaryOperation>
+		{
+		private:
+			using TraitProvider = TransformIteratorTraitProvider<WrappedIterator, UnaryOperation>;
+
+		public:
+
+			// Iterator types
+			using typename TraitProvider::WrappedIteratorType;
+			using typename TraitProvider::value_type;
+			using typename TraitProvider::difference_type;
+			using typename TraitProvider::pointer;
+			using typename TraitProvider::reference;
+			using typename TraitProvider::iterator_category;
+
+			/// <summary>
+			/// Moves the iterator forward n steps.
+			/// Only available if the wrapped iterator is a random access iterator.
+			/// </summary>
+			/// <param name="n"> The number of steps. </return>
+			/// <return> This iterator at its new position. </return>
+			CrtpChildIterator& operator+=(difference_type n)
+			{
+				getCrtpThis()->getWrappedIterator() += n;
+				return *this;
+			}
+
+			/// <summary>
+			/// Moves the iterator backward n steps.
+			/// Only available if the wrapped iterator is a random access iterator.
+			/// </summary>
+			/// <param name="n"> The number of steps. </return>
+			/// <return> This iterator at its new position. </return>
+			CrtpChildIterator& operator-=(difference_type n)
+			{
+				getCrtpThis()->getWrappedIterator() -= n;
+				return *this;
+			}
+
+			/// <summary>
+			/// Returns a new the iterator n steps forward from this one.
+			/// Only available if the wrapped iterator is a random access iterator.
+			/// </summary>
+			/// <param name="n"> The number of steps. </return>
+			/// <return> This iterator at its new position. </return>
+			[[nodiscard]]
+			CrtpChildIterator operator+(difference_type n) const
+			{
+				WrappedIterator tempIt = getCrtpThis()->getWrappedIterator();
+				tempIt += n;
+				return CrtpChildIterator(tempIt, m_transform);
+			}
+
+			/// <summary>
+			/// Returns a new the iterator n steps forward from this one.
+			/// Only available if the wrapped iterator is a random access iterator.
+			/// </summary>
+			/// <param name="lhs"> The number of steps. </return>
+			/// <param name="rhs"> The starting point. </return>
+			/// <return> This iterator at its new position. </return>
+			[[nodiscard]]
+			friend CrtpChildIterator operator+(difference_type lhs, const CrtpChildIterator& rhs)
+			{
+				return rhs + lhs;
+			}
+
+			/// <summary>
+			/// Returns a new the iterator n steps backward from this one.
+			/// Only available if the wrapped iterator is a random access iterator.
+			/// </summary>
+			/// <param name="n"> The number of steps. </return>
+			/// <return> This iterator at its new position. </return>
+			[[nodiscard]]
+			CrtpChildIterator operator-(difference_type n) const
+			{
+				WrappedIterator tempIt = getCrtpThis()->getWrappedIterator();
+				tempIt -= n;
+				return CrtpChildIterator(tempIt, m_transform);
+			}
+
+			/// <summary>
+			/// Apply the transform to the wrapped iterator n steps forward from its current position and return the result.
+			/// This iterator is not moved.
+			/// Only available if the wrapped iterator is a random access iterator.
+			/// 
+			/// Behavior is undefined if  the lifetime of the result of the unary operation is dependent on the
+			/// lifetime of the wrapped iterator.
+			/// </summary>
+			/// <param name="n"> The number of steps. </return>
+			/// <return>  The result of applying the unary operation to the wrapped iterator n steps forward from its current position.  </return>
+			[[nodiscard]]
+			reference operator[](difference_type n)
+			{
+				CrtpChildIterator temp = *getCrtpThis();
+				temp += n;
+				return *temp;
+			}
+
+		private:
+			[[nodiscard]]
+			CrtpChildIterator* getCrtpThis()
+			{
+				return static_cast<CrtpChildIterator*>(this);
+			}
+		};
+	}
+
+	/// <summary>
+	/// Wraps an input iterator and applies a transform to it whenever it is dereferenced.
+	/// 
+	/// TransormIterator is never more than an input iterator even if the wrapped iterator
+	/// is a bidirectional or random access iterator. TransformIterator provides convenience functions
+	/// when wrapping bidirectional and random access iterators, but never fully fulfills the requirements of
+	/// these iterator types.
+	/// </summary>
+	template <class Iterator, typename UnaryOperation>
+	class TransformIterator : public Detail::TransformIteratorRandomAccessHelper<TransformIterator<Iterator, UnaryOperation>, Iterator, UnaryOperation>
+	{
+	private:
+		using TraitProvider = Detail::TransformIteratorTraitProvider<Iterator, UnaryOperation>;
+
+	public:
+
+		// Iterator types
+		using typename TraitProvider::WrappedIteratorType;
+		using typename TraitProvider::value_type;
+		using typename TraitProvider::difference_type;
+		using typename TraitProvider::pointer;
+		using typename TraitProvider::reference;
+		using typename TraitProvider::iterator_category;
+
+		/// <summary>
+		/// Constructor:
+		/// Wraps the provided iterator and applies a transform to it on every dereference
+		/// </summary>
+		/// <param name="wrapped"> The iterator to be wrapped.</param>
+		/// <param name="transform"> function applied to the wrapped iterator whenever this TransformIterator is dereferenced.</param>
+		TransformIterator(Iterator wrapped, UnaryOperation transform) :
+			m_wrappedIt(std::move(wrapped)),
+			m_transform(std::move(transform))
+		{
+		}
+
+		/// <summary>
+		/// Converts this TransformIterator to the wrapped iterator
+		/// </summary>
+		[[nodiscard]]
+		explicit operator WrappedIteratorType()
+		{
+			return m_wrappedIt;
+		}
+
+		/// <summary>
+		/// Gets the iterator wrapped by this transform iterator
+		/// </summary>
+		/// <return> The iterator wrapped by this transform iterator. </return>
+		[[nodiscard]]
+		WrappedIteratorType& getWrappedIterator()
+		{
+			return m_wrappedIt;
+		}
+
+		/// <summary>
+		/// Apply the transform to the wrapped iterator and return the result
+		/// </summary>
+		/// <return> The result of applying the unary operation to the wrapped iterator. </return>
+		[[nodiscard]]
+		reference operator*() const
+		{
+			return std::invoke(m_transform, m_wrappedIt);
+		}
+
+		/// <summary>
+		/// Compare transform iterators for equality.
+		/// </summary>
+		/// <return> True if the wrapped iterators are equal. False otherwise. </return>
+		[[nodiscard]]
+		friend bool operator==(const TransformIterator& lhs, const TransformIterator& rhs)
+		{
+			return lhs.m_wrappedIt == rhs.m_wrappedIt;
+		}
+
+		/// <summary>
+		/// Compare with wrapped iterator for equality. 
+		/// </summary>
+		/// <return> True if the wrapped iterator is equal to the other iterator. False otherwise. </return>
+		[[nodiscard]]
+		friend bool operator==(const TransformIterator& lhs, const WrappedIteratorType& rhs)
+		{
+			return lhs.m_wrappedIt == rhs;
+		}
+
+		/// <summary>
+		/// Compare with wrapped iterator for equality. 
+		/// </summary>
+		/// <return> True if the wrapped iterator is equal to the other iterator. False otherwise. </return>
+		[[nodiscard]]
+		friend bool operator==(const WrappedIteratorType& lhs, const TransformIterator& rhs)
+		{
+			return lhs == rhs.m_wrappedIt;
+		}
+
+		/// <summary>
+		/// Compare transform iterators for inequality.
+		/// </summary>
+		/// <return> True if the wrapped iterators are not equal. False otherwise. </return>
+		[[nodiscard]]
+		friend bool operator!=(const TransformIterator& lhs, const TransformIterator& rhs)
+		{
+			return lhs.m_wrappedIt != rhs.m_wrappedIt;
+		}
+
+		/// <summary>
+		/// Compare with wrapped iterator for inequality. 
+		/// </summary>
+		/// <return> True if the wrapped iterator is not equal to the other iterator. False otherwise. </return>
+		[[nodiscard]]
+		friend bool operator!=(const TransformIterator& lhs, const WrappedIteratorType& rhs)
+		{
+			return lhs.m_wrappedIt != rhs;
+		}
+
+		/// <summary>
+		/// Compare with wrapped iterator for inequality. 
+		/// </summary>
+		/// <return> True if the wrapped iterator is not equal to the other iterator. False otherwise. </return>
+		[[nodiscard]]
+		friend bool operator!=(const WrappedIteratorType& lhs, const TransformIterator& rhs)
+		{
+			return rhs.m_wrappedIt != lhs;
+		}
+
+		/// <summary>
+		/// Moves the iterator forward.
+		/// </summary>
+		/// <return> The iterator after being moved forward. </return>
+		TransformIterator& operator++()
+		{
+			++m_wrappedIt;
+			return *this;
+		}
+
+		/// <summary>
+		/// Moves the iterator forward.
+		/// </summary>
+		/// <return> A new iterator at the position of the original before it was moved forward. </return>
+		[[nodiscard]]
+		TransformIterator operator++(int)
+		{
+			TransformIterator out(*this);
+			++(*this);
+			return out;
+		}
+
+	private:
+		WrappedIteratorType m_wrappedIt;
+		UnaryOperation m_transform;
+	};
+}
---
+++ TransformIteratorTests.cpp
@@ -0,0 +1,258 @@
+﻿#define CATCH_CONFIG_MAIN
+
+#include "catch2/catch.hpp"
+#include "TransformIterator.h"
+
+#include <forward_list>
+#include <list>
+#include <string>
+#include <vector>
+
+struct TransformFunctor
+{
+	template <class Iterator>
+	int operator()(const Iterator& input) const
+	{
+		return (*input) + 1;
+	}
+};
+
+TEST_CASE("Basic transform operations behave correctly", "[TransformIterator]")
+{
+	TransformFunctor transform;
+	std::vector<int> container = { 1, 2, 3, 4, 5 };
+	std::vector<int>::iterator beginIt{ container.begin() };
+	std::vector<int>::iterator endIt{ container.end() };
+
+	SECTION("TransformIterator wraps the expected iterator")
+	{
+		lagy::TransformIterator transformIt(beginIt, [](auto& it) {return *it; });
+		REQUIRE(*transformIt == *beginIt);
+
+		lagy::TransformIterator transformIt2(beginIt + 1, [](auto& it) {return *it; });
+		REQUIRE(*transformIt2 == *(beginIt + 1));
+	}
+
+	SECTION("TransformIterator applies transform on dereference")
+	{
+		auto iteratorToWrap = beginIt;
+
+		lagy::TransformIterator transformIt(iteratorToWrap, transform);
+		REQUIRE(*transformIt == transform(iteratorToWrap));
+	}
+
+	SECTION("TransformIterator correctly casts to wrapped iterator type")
+	{
+		auto iteratorToWrap = beginIt;
+
+		lagy::TransformIterator transformIt(iteratorToWrap, transform);
+		REQUIRE(iteratorToWrap == (decltype(iteratorToWrap))transformIt);
+	}
+
+	SECTION("Two TransformIterators are equal when their respective wrapped iterators are equal")
+	{
+		auto iteratorToWrap = beginIt;
+
+		lagy::TransformIterator transformIt(iteratorToWrap, transform);
+		lagy::TransformIterator transformIt2(iteratorToWrap, transform);
+		REQUIRE(transformIt == transformIt2);
+	}
+
+	SECTION("Two TransformIterators are not equal when their respective wrapped iterators are not equal")
+	{
+		lagy::TransformIterator transformIt(beginIt, transform);
+		lagy::TransformIterator transformIt2(endIt, transform);
+		REQUIRE(!(transformIt == transformIt2));
+	}
+
+	SECTION("A TransformIterator is equal to an instance of WrappedIteratorType if the WrappedIteratorType is equal to the TransformIterator's wrapped iterator")
+	{
+		auto iteratorToWrap = beginIt;
+
+		lagy::TransformIterator transformIt(iteratorToWrap, transform);
+		REQUIRE(transformIt == iteratorToWrap);
+	}
+
+	SECTION("A TransformIterator is not equal to an instance of WrappedIteratorType if the instance is not equal to the TransformIterator's wrapped iterator")
+	{
+		lagy::TransformIterator transformIt(beginIt, transform);
+		REQUIRE(!(transformIt == endIt));
+	}
+
+	SECTION("A TransformIterator is not unequal to another TransformIterator if they both wrap the equal iterators")
+	{
+		auto iteratorToWrap = beginIt;
+
+		lagy::TransformIterator transformIt(iteratorToWrap, transform);
+		lagy::TransformIterator transformIt2(iteratorToWrap, transform);
+		REQUIRE(!(transformIt != transformIt2));
+	}
+
+	SECTION("A TransformIterator is unequal to another TransformIterator if they do not both wrap equal iterators")
+	{
+		lagy::TransformIterator transformIt(beginIt, transform);
+		lagy::TransformIterator transformIt2(endIt, transform);
+		REQUIRE(transformIt != transformIt2);
+	}
+
+	SECTION("A transformIterator is not unequal to an instance of WrappedIteratorType if the instance is equal to the wrapped iterator")
+	{
+		auto iteratorToWrap = beginIt;
+
+		lagy::TransformIterator transformIt(iteratorToWrap, transform);
+		REQUIRE(!(transformIt != iteratorToWrap));
+	}
+
+	SECTION("A TransformIterator is unequal to an instance of WrappedIteratorType if the instance is unequal to the wrapped iterator")
+	{
+		lagy::TransformIterator transformIt(beginIt, transform);
+		REQUIRE(transformIt != endIt);
+	}
+
+	SECTION("TransformIterator is correctly moved by prefix increment")
+	{
+		lagy::TransformIterator transformIt(beginIt, transform);
+		auto& result = ++transformIt;
+
+		// the iterator is moved forward
+		REQUIRE(*transformIt == transform(++container.begin()));
+
+		// the returned iterator is a reference to the original and should also be moved forward
+		REQUIRE(&result == &transformIt);
+		REQUIRE(*result == transform(++container.begin()));
+	}
+
+	SECTION("TransformIterator is correctly moved by postfix increment")
+	{
+		lagy::TransformIterator transformIt(beginIt, transform);
+		auto result = transformIt++;
+
+		// the iterator is moved forward
+		REQUIRE(*transformIt == transform(++container.begin()));
+
+		// the returned iterator at the original position
+		REQUIRE(*result == transform(container.begin()));
+	}
+
+	// Ensure that TransformIterator works with algorithms
+	SECTION("TransformIterator works with standard algorithms")
+	{
+		lagy::TransformIterator transformBegin(beginIt, transform);
+		lagy::TransformIterator transformEnd(endIt, transform);
+
+		std::vector<int> outputVec;
+		std::for_each(transformBegin, transformEnd, [&](const auto& i)
+		{
+			outputVec.push_back(i);
+		});
+
+		std::transform(beginIt, endIt, beginIt, [](auto i) {return i + 1; });
+		REQUIRE(std::equal(outputVec.begin(), outputVec.end(), beginIt));
+	}
+
+	SECTION("TransformIterator works with vector insert")
+	{
+		lagy::TransformIterator transformBegin(beginIt, transform);
+		lagy::TransformIterator transformEnd(endIt, transform);
+
+		std::vector<int> outputVec;
+		outputVec.insert(std::end(outputVec), transformBegin, transformEnd);
+
+		std::transform(beginIt, endIt, beginIt, [](auto i) {return i + 1; });
+		REQUIRE(std::equal(outputVec.begin(), outputVec.end(), beginIt));
+	}
+}
+
+struct functor2
+{
+	template <typename T>
+	int operator()(T& input)
+	{
+		return *input;
+	}
+};
+
+TEST_CASE("TransformIterator works with Bidirectional iterators", "[TransformIterator][IteratorTraits]")
+{
+	 std::list<std::string> list = { "abcd", "efgh", "ijkl" };
+	auto listTransform = [](auto& input) -> std::string&
+	{
+		input->append("zzz");
+		return *input;
+	};
+
+	lagy::TransformIterator transformBegin(std::begin(list), listTransform);
+
+	SECTION("Dereference bidirectional transform iterator")
+	{
+		auto out = *transformBegin;
+		REQUIRE(out.find("zzz") != std::string::npos);
+	}
+
+	SECTION("prefix increment bidirectional transform iterator")
+	{
+		++transformBegin;
+		auto out = *transformBegin;
+		REQUIRE(out == *(++list.begin()));
+	}
+
+	SECTION("postfix increment bidirectional transform iterator")
+	{
+		auto oldPosition = transformBegin++;
+		auto out = *transformBegin;
+		REQUIRE(out == *(++list.begin()));
+		REQUIRE(oldPosition == list.begin());
+	}
+
+	SECTION("prefix decrement bidirectional transform iterator")
+	{
+		++transformBegin;
+		--transformBegin;
+
+		// auto out = *transformBegin;
+		// REQUIRE(out == *(list.begin()));
+	}
+
+	SECTION("postfix decrement bidirectional transform iterator")
+	{
+		//++transformBegin;
+		//auto oldPosition = transformBegin--;
+		//auto out = *transformBegin;
+		//REQUIRE(out == *(list.begin()));
+		//REQUIRE(oldPosition == ++list.begin());
+	}
+}
+
+
+//TEST_CASE("TransformIterator works with forward iterators", "[TransformIterator][IteratorTraits]")
+//{
+//	std::forward_list<std::string> forwardList = { "abcd", "efgh", "ijkl" };
+//	std::string temp;
+//	auto forwardListTransform = [&temp](std::forward_list<std::string>::iterator& input) mutable -> std::string
+//	{
+//		/*input->append("xyz");
+//		return *input;*/
+//		return temp;
+//	};
+//	lagy::TransformIterator transformBegin(std::begin(forwardList), forwardListTransform);
+//	lagy::TransformIterator transformEnd(std::end(forwardList), forwardListTransform);
+//
+//	lagy::TransformIterator tempIt(std::begin(forwardList), functor2{});
+//
+//	SECTION("TransformIterator supports forward iterator operations with forward iterators")
+//	{
+//		REQUIRE(std::is_same_v<std::iterator_traits<decltype(transformBegin)>::iterator_category, std::forward_iterator_tag>);
+//		REQUIRE(std::is_lvalue_reference_v<std::iterator_traits<decltype(transformBegin)>::reference>);
+//	}
+//
+//	SECTION("TransformIterator works with prefix increment")
+//	{
+//		++tempIt;
+//		*tempIt;
+//		// REQUIRE( == "efghxyz");
+//		/*std::for_each(transformBegin, transformEnd, [](auto& instance) {
+//			instance->find("xyz");
+//		});*/
+//	}
+//}
+
